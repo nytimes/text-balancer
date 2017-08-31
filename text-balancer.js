6f2b7a729b0dc178a9fdebcb1510c36f5d67996b
Jamie McCarthy <jamie.mccarthy@voxmedia.com>
2017-08-31 10:40:50 +0200

    Don't bother checking line count, try to squeeze everything
    Manipulating the DOM to determine element height is a clever idea, but
    it's a premature optimization:
    
    (1) it's additional complexity,
    
    (2) the 10 pixel fudge factor leaves its behavior on small text
    questionable,
    
    (3) this implementation doesn't work when a div's html starts with tags
    instead of text, because the first split() word ends up nonvisible so
    firstWord.offsetHeight is zero, and
    
    (4) it's faster to simply try squeezing and see what happens. In my
    rudimentary testing, headlineIsMultipleLines ran in 2.66ms, but simply
    running squeezeContainer completed in 2.73ms, so it's making things
    slower. My numbers may not be super accurate, but they would have to be
    very far off for my conclusion to be backwards.

--- text-balancer.js
+++ text-balancer.js
@@ -61,12 +61,8 @@ var balanceText = function () {
 
     for (i = 0; i < candidates.length; i += 1) {
         element = candidates[i];
-
-        if (textElementIsMultipleLines(element)) {
-            element.style.maxWidth = '';
-            squeezeContainer(element, element.clientHeight, 0, element.clientWidth);
-        }
-
+        element.style.maxWidth = '';
+        squeezeContainer(element, element.clientHeight, 0, element.clientWidth);
     }
 
 }
@@ -89,66 +85,5 @@ var squeezeContainer = function (element, originalHeight, bottomRange, topRange)
         squeezeContainer(element, originalHeight, bottomRange+1, mid);
     }
 }
-
-// function to see if a headline is multiple lines
-// we only want to break if the headline is multiple lines
-//
-// We achieve this by turning the first word into a span
-// and then we compare the height of that span to the height
-// of the entire headline. If the headline is bigger than the
-// span by 10px we balance the headline.
-var textElementIsMultipleLines = function (element) {
-    var firstWordHeight;
-    var elementHeight;
-    var HEIGHT_OFFSET;
-    var elementWords;
-    var firstWord;
-    var ORIGINAL_ELEMENT_TEXT;
-
-    ORIGINAL_ELEMENT_TEXT = element.innerHTML;
-
-    // usually there is around a 5px discrepency between
-    // the first word and the height of the whole headline
-    // so subtract the height of the headline by 10 px and
-    // we should be good
-    HEIGHT_OFFSET = 10;
-
-    // get all the words in the headline as
-    // an array -- will include punctuation
-    //
-    // this is used to put the headline back together
-    elementWords = element.innerHTML.split(' ');
-
-    // make span for first word and give it an id
-    // so we can access it in le dom
-    firstWord = document.createElement('span');
-    firstWord.id = 'element-first-word';
-    firstWord.innerHTML = elementWords[0];
-
-    // this is the entire headline
-    // as an array except for first word
-    //
-    // we will append it to the headline after the span
-    elementWords = elementWords.slice(1);
-
-    // empty the headline and append the span to it
-    element.innerHTML = '';
-    element.appendChild(firstWord);
-
-    // add the rest of the element back to it
-    element.innerHTML += ' ' + elementWords.join(' ');
-
-    // update the first word variable in the dom
-    firstWord = document.getElementById('element-first-word');
-
-    firstWordHeight = firstWord.offsetHeight;
-    elementHeight = element.offsetHeight;
-    // restore the original element text
-    element.innerHTML = ORIGINAL_ELEMENT_TEXT;
-
-    // compare the height of the element and the height of the first word
-    return elementHeight - HEIGHT_OFFSET > firstWordHeight;
-
-} // end headlineIsMultipleLines
 
 exports.balanceText = textBalancer;
